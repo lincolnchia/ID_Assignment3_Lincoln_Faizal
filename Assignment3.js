53442d72a5b07b59961ec44426829a2ab5cb5efc
lincolnchia <73088199+lincolnchia@users.noreply.github.com>
2021-02-09 15:34:00 +0800

    Adding of data to api

--- Assignment3.js
+++ Assignment3.js
@@ -1,19 +1,33 @@
-// document.getElementById('submit_button').addEventListener('click', function(){
 //     const searchdata = document.getElementById('input-country').value
 //     convertedData = searchdata.replace(/\s/g,"%20")
     // fetch("https://restcountries.eu/rest/v2/name/"+searchdata+"fullText=true")
+function findcountries(){
     fetch("https://restcountries.eu/rest/v2/all")
-    .then(function(res){
-        return res.json();
+    .then(res=>res.json())
+    .then(data=>{
+        for(i=0;i<data.length;i++){
+            if(document.getElementById('input-country').value == data[i].name){
+                $("#flag-container").empty()
+                $("#flag-container").append('<img src="'+ data[i].flag+'">')
+                $("#capital").empty();
+                $("#capital").append(data[i].capital);
+                $("#dialing-code").empty();
+                $("#dialing-code").append("+" + data[i].callingCodes);
+                $("#timezones").empty();
+                $("#timezones").append(data[i].timezones);
+                $("#currencies").empty();
+                console.log(data);
+                $("#currencies").append(data[i].currencies[0].name +" (" + data[i].currencies[0].symbol + ")");
+                $("#region").empty();
+                $("#region").append(data[i].region);
+                $("#amountOfLand").empty();
+                $("#amountOfLand").append(data[i].area);
+            }
+        }
     })
-    .then(function(data){
-        console.log(data);
-        initalize(data)
-    })
-    .catch(function(err){
-        console.log("Error: " + err)
-    })
+    
+}
 
-    function initalize(countrydata){
-        console.log(countrydata)
-    }
+
+
+
