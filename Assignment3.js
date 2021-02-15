9768ba1ca0c3df8559f35a0c0085be53b8415065
lincolnchia <73088199+lincolnchia@users.noreply.github.com>
2021-02-15 18:25:58 +0800

    Adding of validation for input

--- Assignment3.js
+++ Assignment3.js
@@ -1,16 +1,19 @@
-//     const searchdata = document.getElementById('input-country').value
-//     convertedData = searchdata.replace(/\s/g,"%20")
-    // fetch("https://restcountries.eu/rest/v2/name/"+searchdata+"fullText=true")
+// Getting input by presssing button
 document.getElementById("submit-btn").addEventListener('click',function(){
-    findcountries();
+    fullInput = document.getElementById('input-country').value;
+    fullInput = fullInputValidation(input);
+    findcountries(input);
+
 })
 
-function findcountries(){
+
+
+function findcountries(input){
     fetch("https://restcountries.eu/rest/v2/all")
     .then(res=>res.json())
     .then(data=>{
         for(i=0;i<data.length;i++){
-            if(document.getElementById('input-country').value == data[i].name){
+            if(input == data[i].name){
                 $("#flag-container").empty()
                 $("#flag-container").append('<img src="'+ data[i].flag+'">')
                 $("#capital").empty();
@@ -31,6 +34,14 @@ function findcountries(){
     })
     
 }
+
+// Making input Capitalized and lowercased properly
+function inputValidation(input){
+    input = input.toLowerCase();
+    input = input.charAt(0).toUpperCase() + input.slice(1);
+    return input;
+}
+
 
 
 
