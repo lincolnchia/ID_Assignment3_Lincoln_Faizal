b4a04da09cd0d2fa423eb4321131f14ce157682b
lincolnchia <73088199+lincolnchia@users.noreply.github.com>
2021-02-19 11:19:08 +0800

    Adding of pop up log in menu with css

--- Js/Assignment3.js
+++ Js/Assignment3.js
@@ -1,3 +1,11 @@
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 // Getting input by presssing button
 document.getElementById("submit-btn").addEventListener('click',function(){
